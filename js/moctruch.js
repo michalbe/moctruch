c33294d99796c466f8c5df8711a905c9075617eb
Michal Budzynski <michal@virtualdesign.pl>
2013-07-14 01:21:05 +0200

    working version

--- js/moctruch.js
+++ js/moctruch.js
@@ -1,57 +1,32 @@
-MOCTRUCH = function(callback, element, options) {
-  var blur = options.blur || 5;
-  var width = element.scrollWidth;
-  var height = element.scrollHeight;
-  
-  var createSVG = function() {
-    var doc = document.implementation.createHTMLDocument("");
-    //doc.write('AAA');
-    doc.body.innerHTML = '<b>AA</b>';
-    doc.documentElement.setAttribute("xmlns", doc.documentElement.namespaceURI);
-    var html = (new XMLSerializer).serializeToString(doc);
+var bigContainer = document.getElementById('big-container');
+var container = document.getElementById('container');
+var output = document.getElementById('output');
+var actions = {};
 
-    var svg = 
-      "<svg xmlns='http://www.w3.org/2000/svg' width='" + width + "' height='" + height + "'>" +
-        "<defs>" +
-          "<filter id='f1' x='0' y='0'>" +
-            "<feGaussianBlur in='SourceGraphic' stdDeviation='15' />" +
-          "</filter>" +
-        "</defs>" +
-      "<foreignObject width='100%' height='100%' filter='url(#f1)'>" +
-        '<body xmlns="http://www.w3.org/1999/xhtml">'+
-             '<h1>A</h1>' + 
-          '</body>'+ 
-      "</foreignObject>" +
-    "</svg>";
-    
-    return svg;
-  }
+var svgOverlay = "<svg xmlns='http://www.w3.org/2000/svg' width='320' height='560'> " + "<defs> " + "<filter id='f1' x='0' y='0'> " + "<feGaussianBlur in='SourceGraphic' stdDeviation='25' /> " + "</filter> " + "</defs> " + "<foreignObject width='100%' height='100%' filter='url(#f1)'> " + bigContainer.innerHTML + "</foreignObject> " + "</svg>";
 
-  var createCanvas = function(blob, cb) {
-    console.log(blob);
-    var canvas = document.createElement('canvas');
-    var ctx = canvas.getContext('2d');
-    canvas.width = width;
-    canvas.height = height;
-    
-    var img = new Image();
-    blob = new Blob([blob], {type: "image/svg+xml;charset=utf-8"});
-    var url = window.URL.createObjectURL(blob);
-    img.onload = function() {
-        ctx.drawImage(img, 0, 0);
-        window.URL.revokeObjectURL(url);
-        cb(canvas);
-    };
-    img.src = url;
-  }
-  
-  createCanvas(createSVG(), callback);
+output.innerHTML = svgOverlay;
+
+actions.blur = function() {
+  container.style.display = 'none';
+  output.style.display = 'block';
 }
 
-MOCTRUCH(
-  function(cvs) { 
-    document.getElementById('output').appendChild(cvs);
-  },
-  document.getElementById('container'), 
-  {blur: 15}
-);
+actions.unblur = function() {
+  output.style.display = 'none';
+  container.style.display = 'block';
+}
+
+actions.animStart = function() {
+  document.body.classList.add('anim');
+}
+
+actions.animStop = function() {
+  document.body.classList.remove('anim');
+}
+
+document.getElementById('controls').addEventListener('click', function(evt) {
+  if (typeof actions[evt.target.id] === 'function') {
+    actions[evt.target.id]();
+  }
+})
